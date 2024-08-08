0ef180e36626b6a8d3ea05bb1eea627f81622cce
IT21826740 <IT21826740@my.sliit.lk>
2024-08-08 17:17:33 +0530

    Create Exception

---
+++ lk/zerocode/DAY_21/RegistrationFeeNotSufficantExecption.java
@@ -0,0 +1,7 @@
+package lk.zerocode.DAY_21;
+
+public class RegistrationFeeNotSufficantExecption extends Exception{
+    public RegistrationFeeNotSufficantExecption(String massage) {
+        super(massage);
+    }
+}
