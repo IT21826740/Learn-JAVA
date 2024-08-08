1707c62c7e065508d8b1e7a52759367582f82eff
IT21826740 <IT21826740@my.sliit.lk>
2024-08-08 17:17:23 +0530

    Create Exception

---
+++ lk/zerocode/DAY_21/StudentNotRegisteredException.java
@@ -0,0 +1,9 @@
+package lk.zerocode.DAY_21;
+
+public class StudentNotRegisteredException extends Exception {
+
+
+    public StudentNotRegisteredException(String massage) {
+        super(massage);
+    }
+}
