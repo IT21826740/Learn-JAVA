59019ce3568ffb37d5ad44d7aeea210ef7d1e842
IT21826740 <IT21826740@my.sliit.lk>
2024-08-12 16:42:57 +0530

    Create main method

---
+++ lk/zerocode/Day_22/Main.java
@@ -0,0 +1,8 @@
+package lk.zerocode.Day_22;
+
+public class Main {
+    public static void main(String[] args) {
+        LoanController lc = new LoanController();
+        lc.inquire("200050404648",50000.00);
+    }
+}
