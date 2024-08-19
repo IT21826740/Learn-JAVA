c150bc8d05f80354f4c4f2377c3b28b96a367263
IT21826740 <IT21826740@my.sliit.lk>
2024-08-19 17:04:51 +0530

    Create Exception class

---
+++ lk/zerocode/Day_23/Exceptions/InvalidAccountNumberException.java
@@ -0,0 +1,8 @@
+package lk.zerocode.Day_23.Exceptions;
+
+public class InvalidAccountNumberException extends Exception{
+    public InvalidAccountNumberException (String message){
+        super(message);
+    }
+
+}
