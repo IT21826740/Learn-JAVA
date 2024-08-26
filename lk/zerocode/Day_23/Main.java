01ca698925d5850b6225c52561aa4679c8c0b7e1
IT21826740 <IT21826740@my.sliit.lk>
2024-08-26 13:38:53 +0530

    Update Main method

--- lk/zerocode/Day_23/Main.java
+++ lk/zerocode/Day_23/Main.java
@@ -1,11 +1,13 @@
 package lk.zerocode.Day_23;
 
+import lk.zerocode.Day_23.Exceptions.AccountBalanceNotSufficentException;
 import lk.zerocode.Day_23.Exceptions.InvalidAccountNumberException;
+import lk.zerocode.Day_23.Exceptions.MinimumBalanceNotSufficentException;
 
 public class Main {
-    public static void main(String[] args) throws InvalidAccountNumberException {
+    public static void main(String[] args) throws InvalidAccountNumberException, AccountBalanceNotSufficentException, MinimumBalanceNotSufficentException {
         AccountService accountService = new AccountService();
-        accountService.transfer("789","456",7000);
+        accountService.transfer("789","456",7000,1000);
 
     }
 }
