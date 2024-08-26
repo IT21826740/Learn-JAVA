4ee7951aa5d26c73b5ef0e5d0e205adfb00d9f86
IT21826740 <IT21826740@my.sliit.lk>
2024-08-26 13:37:28 +0530

    Create Exception class

---
+++ lk/zerocode/Day_23/Exceptions/AccountBalanceNotSufficentException.java
@@ -0,0 +1,9 @@
+package lk.zerocode.Day_23.Exceptions;
+
+public class AccountBalanceNotSufficentException extends Exception{
+
+    public AccountBalanceNotSufficentException (String message){
+
+        super();
+    }
+}
---
+++ lk/zerocode/Day_23/Exceptions/MinimumBalanceNotSufficentException.java
@@ -0,0 +1,8 @@
+package lk.zerocode.Day_23.Exceptions;
+
+public class MinimumBalanceNotSufficentException extends Exception{
+    public MinimumBalanceNotSufficentException (String message){
+
+       super();
+    }
+}
